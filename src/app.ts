50c695208e5aef22afe67ef33e9b9caa1179c2dc
khamzaevasad <xamzayevasad442@gmail.com>
2025-10-24 04:09:41 +0900

    Feat: integrate middleware session

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,14 @@ import router from "./router";
 import routerAdmin from "./router-Admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
+import session from "express-session";
+import ConnectMongoDBSession from "connect-mongodb-session";
+
+const MongodbStore = ConnectMongoDBSession(session);
+const store = new MongodbStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "session",
+});
 
 /** 1-Entrance **/
 const app = express();
@@ -13,6 +21,18 @@ app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
 /** 2-Session **/
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 3, //3h
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
 /** 3-Views **/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
