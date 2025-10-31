e50ecb9cd1f18c99313e35cdde9feb235d668388
khamzaevasad <xamzayevasad442@gmail.com>
2025-10-31 21:46:23 +0900

    fix: update getUser method to throw an error when no users are found

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -103,7 +103,8 @@ class MemberService {
       .find({ memberType: MemberType.USER })
       .exec();
 
-    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    if (!result.length)
+      throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
     return result;
   }
   // updateChosenUser
