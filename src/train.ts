b0016b28bda776e5b9a2b631c1650c81ef5a4675
khamzaevasad <xamzayevasad442@gmail.com>
2025-11-08 03:29:54 +0900

    Feat: add function to check if an object has a specific property

--- src/train.ts
+++ src/train.ts
@@ -190,5 +190,15 @@ P-TASK:
 Shunday function yozing, u object qabul qilsin va arrayni object arrayga otkazib arrayni qaytarsin qaytarsin.
 MASALAN: objectToArray( {a: 10, b: 20}) return [['a', 10], ['b', 20]] 
 */
-const objectToArray = (obj: {}): unknown[] => Object.entries(obj);
-console.log(objectToArray({ a: 10, b: 20 }));
+// const objectToArray = (obj: {}): unknown[] => Object.entries(obj);
+// console.log(objectToArray({ a: 10, b: 20 }));
+
+/*
+Shunday function yozing, u 2 ta parametrgga ega bolib birinchisi object, ikkinchisi string. Agar string parametr objectni propertysi bolsa true bolmasa false qaytarsin.
+MASALAN: hasProperty({name: "BMW", model: "M3"}, "model") return true; hasProperty({name: "BMW", model: "M3"}, "year") return false
+*/
+
+function hasProperty(obj: Record<string, any>, key: string): boolean {
+  return obj.hasOwnProperty(key);
+}
+console.log(hasProperty({ name: "BMW", model: "M3" }, "model"));
